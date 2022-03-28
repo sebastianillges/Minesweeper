781018d21891d32fb1c40da88151d74e33390ff1
kaicikoglu <kaicikoglu@outlook.de>
2022-03-28 17:01:11 +0200

    centered matchfield on console and rearranged methodes

--- src/main/scala/Minesweeper.scala
+++ src/main/scala/Minesweeper.scala
@@ -2,18 +2,25 @@
   println(eol + "Minesweeper matchfield:" + eol)
   println(matchfield(8, 6, 3))
 
-def eol = sys.props("line.separator")
+def matchfield(height: Int = 3, width: Int = 3, cellWidth: Int = 3) =
+  ("   " * 25) + firstHorizontal(cellWidth, width)
+    + (("   " * 25) + vertical(cellWidth, width) * (cellWidth / 2))
+    + ((("   " * 25) + horizontal(cellWidth, width) + ("   " * 25) + vertical(
+      cellWidth,
+      width
+    ) * (cellWidth / 2)) * (height - 1))
+    + ("   " * 25) + lastHorizontal(cellWidth, width)
 
 def firstHorizontal(cellWidth: Int = 3, cellNum: Int = 3) =
   "┌" + (("─" * cellWidth) + "┬") * (cellNum - 1) + ("─" * cellWidth) + "┐" + eol
+
 def horizontal(cellWidth: Int = 3, cellNum: Int = 3) =
   "├" + (("─" * cellWidth) + "┼") * (cellNum - 1) + ("─" * cellWidth) + "┤" + eol
+
 def lastHorizontal(cellWidth: Int = 3, cellNum: Int = 3) =
   "└" + (("─" * cellWidth) + "┴") * (cellNum - 1) + ("─" * cellWidth) + "┘" + eol
+
 def vertical(cellHeight: Int = 3, cellNum: Int = 3) =
   ("│" + (" " * cellHeight)) * cellNum + "│" + eol
-def matchfield(height: Int = 3, width: Int = 3, cellWidth: Int = 3) =
-  ("   " * 20) + firstHorizontal(cellWidth, width)
-      + (("   " * 20) + vertical(cellWidth, width) * (cellWidth / 2))
-      + ((("   " * 20) + horizontal(cellWidth, width) + ("   " * 20) + vertical(cellWidth, width) * (cellWidth / 2)) * (height - 1))
-      + ("   " * 20) + lastHorizontal(cellWidth, width)
+
+def eol = sys.props("line.separator")
