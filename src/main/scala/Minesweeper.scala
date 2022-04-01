d2cbc7de32efe305bfc8d9f969724403a20de42b
kaicikoglu <kaicikoglu@outlook.de>
2022-04-01 12:42:56 +0200

    center matchfield

--- src/main/scala/Minesweeper.scala
+++ src/main/scala/Minesweeper.scala
@@ -3,13 +3,13 @@
   println(matchfield(8, 6, 3))
 
 def matchfield(height: Int = 3, width: Int = 3, cellWidth: Int = 3) =
-  ("   " * 25) + firstHorizontal(cellWidth, width)
-    + (("   " * 25) + vertical(cellWidth, width) * (cellWidth / 2))
-    + ((("   " * 25) + horizontal(cellWidth, width) + ("   " * 25) + vertical(
+  ("   " * 23) + firstHorizontal(cellWidth, width)
+    + (("   " * 23) + vertical(cellWidth, width) * (cellWidth / 2))
+    + ((("   " * 23) + horizontal(cellWidth, width) + ("   " * 23) + vertical(
       cellWidth,
       width
     ) * (cellWidth / 2)) * (height - 1))
-    + ("   " * 25) + lastHorizontal(cellWidth, width)
+    + ("   " * 23) + lastHorizontal(cellWidth, width)
 
 def firstHorizontal(cellWidth: Int = 3, cellNum: Int = 3) =
   "┌" + (("─" * cellWidth) + "┬") * (cellNum - 1) + ("─" * cellWidth) + "┐" + eol
