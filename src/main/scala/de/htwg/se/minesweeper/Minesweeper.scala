ffda3785b629fe2824e254012cfb0709c2953725
kaicikoglu <kaicikoglu@outlook.de>
2022-04-03 23:51:46 +0200

    create Worksheet

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -3,5 +3,8 @@ import de.htwg.se.minesweeper.model.*
 @main def main(): Unit =
   println("Minesweeper matchfield: \n")
 
-  val field = new Field(6, 5, Stone.Bomb)
-  println(field.matchfield())
+  val field = new Field(3, 3, Stone.Flag)
+
+  println(field.horizontal())
+  println()
+  println(field.lastHorizontal())
