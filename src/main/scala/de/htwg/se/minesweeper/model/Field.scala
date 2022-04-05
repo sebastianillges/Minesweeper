dcd979da6f2f1f40112b05f383f76c8f2d446bca
kaicikoglu <kaicikoglu@outlook.de>
2022-04-05 22:55:46 +0200

    renamed Variables

--- src/main/scala/de/htwg/se/minesweeper/model/Field.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/Field.scala
@@ -3,52 +3,45 @@ package de.htwg.se.minesweeper.model
 import scala.language.postfixOps
 
 case class Field(matrix: Matrix[Stone]):
-  def this(rows: Int = 3, cols: Int = 3, filling: Stone = Stone.Flag) = this(new Matrix[Stone](rows, cols, filling))
+  def this(rows: Int = 3, cols: Int = 3, filling: Stone = Stone.NotTracked) =
+    this(new Matrix[Stone](rows, cols, filling))
 
   val cols: Int = matrix.colNum
   val rows: Int = matrix.rowNum
   val eol: String = sys.props("line.separator")
 
-  def firstHorizontal(cellWidth: Int = 3, row: Int = rows): String =
+  def firstBar(cellWidth: Int = 3, row: Int = rows): String =
     if (cellWidth % 2 == 0) {
       "┌" + (("─" * (cellWidth - 1)) + "┬") * (row - 1) + ("─" * (cellWidth - 1)) + "┐" + eol
     } else {
       "┌" + (("─" * cellWidth) + "┬") * (row - 1) + ("─" * cellWidth) + "┐" + eol
     }
 
-  def horizontal(cellWidth: Int = 3, row: Int = rows): String =
+  def bar(cellWidth: Int = 3, row: Int = rows): String =
     if (cellWidth % 2 == 0) {
       "├" + (("─" * (cellWidth - 1)) + "┼") * (row - 1) + ("─" * (cellWidth - 1)) + "┤" + eol
     } else {
       "├" + (("─" * cellWidth) + "┼") * (row - 1) + ("─" * cellWidth) + "┤" + eol
     }
 
-  def lastHorizontal(cellWidth: Int = 3, row: Int = rows): String =
+  def lastBar(cellWidth: Int = 3, row: Int = rows): String =
     if (cellWidth % 2 == 0) {
       "└" + (("─" * (cellWidth - 1)) + "┴") * (row - 1) + ("─" * (cellWidth - 1)) + "┘" + eol
     } else {
       "└" + (("─" * cellWidth) + "┴") * (row - 1) + ("─" * cellWidth) + "┘" + eol
     }
 
-  def vertical(row: Int = rows, cellWidth: Int = 3): String =
-    if (row == 1) {
-      matrix
-        .row(row - 1)
-        .map(_.toString)
-        .map(" " * ((cellWidth - 1) / 2) + _ + " " * ((cellWidth - 1) / 2))
-        .mkString("│", "│", "│") + eol
-    } else {
-      matrix
-        .row(row)
-        .map(_.toString)
-        .map(" " * ((cellWidth - 1) / 2) + _ + " " * ((cellWidth - 1) / 2))
-        .mkString("│", "│", "│") + eol
-    }
+  def cells(row: Int, cellWidth: Int = 3): String =
+    matrix
+      .row(row)
+      .map(_.toString)
+      .map(" " * ((cellWidth - 1) / 2) + _ + " " * ((cellWidth - 1) / 2))
+      .mkString("│", "│", "│") + eol
 
   def matchfield(cellWidth: Int = 3): String =
     (0 until rows)
-      .map(vertical(_, cellWidth))
-      .mkString(firstHorizontal(cellWidth, cols), horizontal(cellWidth, cols), lastHorizontal(cellWidth, cols))
+      .map(cells(_, cellWidth))
+      .mkString(firstBar(cellWidth, cols), bar(cellWidth, cols), lastBar(cellWidth, cols))
 
   override def toString: String = matchfield()
   def put(stone: Stone, x: Int, y: Int): Field = copy(matrix.replaceCell(x, y, stone))
