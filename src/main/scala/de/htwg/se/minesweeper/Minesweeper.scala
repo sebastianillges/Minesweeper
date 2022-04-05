94c224664f5decbbf0ef4fa0c80843dbb7f6cf5b
kaicikoglu <kaicikoglu@outlook.de>
2022-04-05 23:01:07 +0200

    merged kai into Development

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -3,8 +3,5 @@ import de.htwg.se.minesweeper.model.*
 @main def main(): Unit =
   println("Minesweeper matchfield: \n")
 
-  val field = new Field(3, 3, Stone.Flag)
-
-  println(field.horizontal())
-  println()
-  println(field.lastHorizontal())
+  val field = new Field(3, 3)
+  println(field.toString)
