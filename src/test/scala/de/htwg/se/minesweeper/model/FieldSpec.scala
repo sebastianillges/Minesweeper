236b2b49799e57c343fe9e17c90220534c94750a
kaicikoglu <kaicikoglu@outlook.de>
2022-04-06 19:28:37 +0200

    included more tests

--- src/test/scala/de/htwg/se/minesweeper/model/FieldSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/FieldSpec.scala
@@ -1,11 +1,74 @@
 package de.htwg.se.minesweeper.model
 
-import de.htwg.se.minesweeper.model.*
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
 class FieldSpec extends AnyWordSpec {
   "A Minesweeper Field" when {
+    "created with default Stone" should {
+      val field1 = new Field(1, 1)
+      val field2 = new Field(1, 2)
+      val field3 = new Field(2, 3)
+      val field = new Field(3, 3)
+      val eol = field1.eol
+      "have a firstBar of form '┌───┬───┬───┐'" in {
+        field.firstBar() should be("┌───┬───┬───┐" + eol)
+      }
+      "have a scalable firstBar with odd cellWidth and even cellWidth parameter" in {
+        field2.firstBar(2, 1) should be("┌─┐" + eol)
+        field2.firstBar(4, 1) should be("┌───┐" + eol)
+        field2.firstBar(6, 3) should be("┌─────┬─────┬─────┐" + eol)
+      }
+      "have a scalable firstBar with odd cellWidth and odd cellWidth parameter" in {
+        field1.firstBar(1, 1) should be("┌─┐" + eol)
+        field1.firstBar(3, 1) should be("┌───┐" + eol)
+        field1.firstBar(5, 3) should be("┌─────┬─────┬─────┐" + eol)
+      }
+      "have a bar of form '├───┼───┼───┤'" in {
+        field.bar() should be("├───┼───┼───┤" + eol)
+      }
+      "have a scalable bar with odd cellWidth and even cellWidth parameter" in {
+        field2.bar(2, 1) should be("├─┤" + eol)
+        field2.bar(4, 1) should be("├───┤" + eol)
+        field2.bar(6, 3) should be("├─────┼─────┼─────┤" + eol)
+      }
+      "have a scalable bar with odd cellWidth and odd cellWidth parameter" in {
+        field1.bar(1, 1) should be("├─┤" + eol)
+        field1.bar(3, 1) should be("├───┤" + eol)
+        field1.bar(5, 3) should be("├─────┼─────┼─────┤" + eol)
+      }
+      "have a lastBar of form '└───┴───┴───┘'" in {
+        field.lastBar() should be("└───┴───┴───┘" + eol)
+      }
+      "have a scalable lastBar with odd cellWidth and even cellWidth parameter" in {
+        field2.lastBar(2, 1) should be("└─┘" + eol)
+        field2.lastBar(4, 1) should be("└───┘" + eol)
+        field2.lastBar(6, 3) should be("└─────┴─────┴─────┘" + eol)
+      }
+      "have a scalable lastBar with odd cellWidth and odd cellWidth parameter" in {
+        field1.lastBar(1, 1) should be("└─┘" + eol)
+        field1.lastBar(3, 1) should be("└───┘" + eol)
+        field1.lastBar(5, 3) should be("└─────┴─────┴─────┘" + eol)
+      }
+      "have cells as String of form '│ \u25A0 │ \u25A0 │ \u25A0 │'" in {
+        field.cells(0) should be("│ \u25A0 │ \u25A0 │ \u25A0 │" + eol)
+      }
+      "have scalable cells" in {
+        field1.cells(0, 1) should be("│\u25A0│" + eol)
+        field2.cells(0, 1) should be("│\u25A0│\u25A0│" + eol)
+        field.cells(0, 3) should be("│ \u25A0 │ \u25A0 │ \u25A0 │" + eol)
+      }
+      "have a matchField in the form " +
+        "┌─┐" +
+        "\u25A0" +
+        "└─┘" in {
+          field1.matchfield(1) should be("┌─┐" + eol + "│\u25A0│" + eol + "└─┘" + eol)
+          field3.matchfield(3) should be(
+            "┌───┬───┬───┐" + eol + "│ \u25A0 │ \u25A0 │ \u25A0 │" + eol + "├───┼───┼───┤" +
+              eol + "│ \u25A0 │ \u25A0 │ \u25A0 │" + eol + "└───┴───┴───┘" + eol
+          )
+        }
+    }
     "filled with ✴" should {
       val field1 = new Field(1, 1, Stone.Bomb)
       val field2 = new Field(1, 2, Stone.Bomb)
