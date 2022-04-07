2009c51aeb5ed6264b1110ff1a6eb0148a1201ab
kaicikoglu <kaicikoglu@outlook.de>
2022-04-07 20:21:58 +0200

    adjusted Field.scala

--- src/main/scala/de/htwg/se/minesweeper/model/Field.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/Field.scala
@@ -6,25 +6,18 @@ case class Field(height: Int = 3, width: Int = 3, cellWidth: Int = 3):
   def eol = sys.props("line.separator")
 
   def firstBar(cellWidth: Int = 3, cellNum: Int = 3) =
-    if (cellWidth % 2 == 0)
-      "┌" + (("─" * (cellWidth - 1)) + "┬") * (cellNum - 1) + ("─" * (cellWidth - 1)) + "┐" + eol
-    else
-      "┌" + (("─" * cellWidth) + "┬") * (cellNum - 1) + ("─" * cellWidth) + "┐" + eol
-
+    "┌" + (("─" * cellWidth) + "┬") * (cellNum - 1) + ("─" * cellWidth) + "┐" + eol
   def bar(cellWidth: Int = 3, cellNum: Int = 3) =
-    if (cellWidth % 2 == 0)
-      "├" + (("─" * (cellWidth - 1)) + "┼") * (cellNum - 1) + ("─" * (cellWidth - 1)) + "┤" + eol
-    else
-      "├" + (("─" * cellWidth) + "┼") * (cellNum - 1) + ("─" * cellWidth) + "┤" + eol
+    "├" + (("─" * cellWidth) + "┼") * (cellNum - 1) + ("─" * cellWidth) + "┤" + eol
   def lastBar(cellWidth: Int = 3, cellNum: Int = 3) =
-    if (cellWidth % 2 == 0)
-      "└" + (("─" * (cellWidth - 1)) + "┴") * (cellNum - 1) + ("─" * (cellWidth - 1)) + "┘" + eol
-    else
-      "└" + (("─" * cellWidth) + "┴") * (cellNum - 1) + ("─" * cellWidth) + "┘" + eol
+    "└" + (("─" * cellWidth) + "┴") * (cellNum - 1) + ("─" * cellWidth) + "┘" + eol
   def cells(cellWidth: Int = 3, cellNum: Int = 3) =
     ("│" + (" " * cellWidth)) * cellNum + "│" + eol
   def matchfield(height: Int, width: Int, cellWidth: Int) =
     firstBar(cellWidth, width)
       + cells(cellWidth, width)
-      + (bar(cellWidth, width) + cells(cellWidth, width) * (height - 1))
+      + (bar(cellWidth, width) + cells(
+        cellWidth,
+        width
+      )) * (height - 1)
       + lastBar(cellWidth, width)
