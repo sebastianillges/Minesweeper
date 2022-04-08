5c8a17f3b4c954ee289852c9610ca788ae12f940
kaicikoglu <kaicikoglu@outlook.de>
2022-04-08 18:17:26 +0200

    changed Matrix.scala

--- src/main/scala/de/htwg/se/minesweeper/Worksheet.sc
+++ src/main/scala/de/htwg/se/minesweeper/Worksheet.sc
@@ -1,1 +1,1 @@
-class TestRDD[T: ClassTag](rdd: RDD[T]) extends RDD[T](rdd: RDD[T])
+
--- src/main/scala/de/htwg/se/minesweeper/model/Matrix.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/Matrix.scala
@@ -2,11 +2,14 @@ package de.htwg.se.minesweeper.model
 
 import scala.reflect.ClassTag
 
-case class Matrix[T: ClassTag](rows: Array[Array[T]]):
-  def this(sizeRow: Int, sizeCol: Int, filling: T) = this(Array.tabulate[T](sizeRow, sizeCol)((row, col) => filling))
+case class Matrix[S, T](rows: Vector[Vector[(S, T)]]):
+  def this(sizeRow: Int, sizeCol: Int, filling: (S, T)) =
+    this(Vector.tabulate[(S, T)](sizeRow, sizeCol)((row, col) => filling))
   val rowNum: Int = rows.length
   val colNum: Int = rows(rowNum - 1).length
-  def cell(row: Int, col: Int): T = rows(row)(col)
-  def row(rowIndex: Int): Array[T] = rows(rowIndex)
-  def fill(filling: T): Matrix[T] = copy(Array.tabulate(rowNum, colNum) { (row, col) => filling })
-  def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(rows.updated(row, rows(row).updated(col, cell)))
+  def cell(row: Int, col: Int): (S, T) = rows(row)(col)
+  def row(rowIndex: Int): Vector[(S, T)] = rows(rowIndex)
+  def fill(filling: (S, T)): Matrix[S, T] = copy(Vector.tabulate(rowNum, colNum) { (row, col) => filling })
+  def replaceCell(row: Int, col: Int, cell: (S, T)): Matrix[S, T] = copy(
+    rows.updated(row, rows(row).updated(col, cell))
+  )
