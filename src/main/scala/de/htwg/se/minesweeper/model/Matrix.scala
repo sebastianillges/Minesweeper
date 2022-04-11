5246b50ab0b4b8369dc3d06462d66a3499360819
kaicikoglu <kaicikoglu@outlook.de>
2022-04-11 19:04:15 +0200

    fixed bug

--- src/main/scala/de/htwg/se/minesweeper/model/Matrix.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/Matrix.scala
@@ -4,7 +4,7 @@ case class Matrix[S, T](rows: Vector[Vector[(S, T)]]):
   def this(sizeRow: Int, sizeCol: Int, filling: (S, T)) =
     this(Vector.tabulate[(S, T)](sizeRow, sizeCol)((row, col) => filling))
   val rowNum: Int = rows.length
-  val colNum: Int = rows(rowNum - 1).length
+  val colNum: Int = rows(0).length
   def cell(row: Int, col: Int): (S, T) = rows(row)(col)
   def row(rowIndex: Int): Vector[(S, T)] = rows(rowIndex)
   def fill(filling: (S, T)): Matrix[S, T] = copy(Vector.tabulate(rowNum, colNum) { (row, col) => filling })
