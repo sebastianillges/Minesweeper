bb16b1b9f22599c58403169d8105511d7bc579f7
kaicikoglu <kaicikoglu@outlook.de>
2022-04-29 12:22:04 +0200

    refactoring

--- src/main/scala/de/htwg/se/minesweeper/controller/Controller.scala
+++ src/main/scala/de/htwg/se/minesweeper/controller/Controller.scala
@@ -1,20 +1,22 @@
 package de.htwg.se.minesweeper.controller
 
+import de.htwg.se.minesweeper.model.Coordinates
 import de.htwg.se.minesweeper.model.Field
-import de.htwg.se.minesweeper.model.Stone
 import de.htwg.se.minesweeper.util.Observable
 
 case class Controller(var field: Field) extends Observable:
 
-  def setBombs(bombNumber: Int) = field.setBombs(bombNumber)
+  def doAndPublish(doThis: Coordinates => Field, move: Coordinates) =
+    field = doThis(move)
+    notifyObservers
 
-  def revealValue(x: Int, y: Int) = field.revealValue(x, y)
+  def revealValue(move: Coordinates) =
+    field.revealValue(move.x, move.y)
 
-  def createFieldWithBombs(param: Int) =
-    if (param == 8) then field = setBombs(10)
-    else if (param == 16) then field = setBombs(40)
-    else if (param == 32) then field = setBombs(99)
-
-  notifyObservers
+  def createFieldWithBombs =
+    if (field.matrix.rowNum == 8) then field = field.setBombs(10)
+    else if (field.matrix.rowNum == 16) then field = field.setBombs(40)
+    else if (field.matrix.rowNum == 32) then field = field.setBombs(99)
+    notifyObservers
 
   override def toString = field.toString
--- src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/controller/ControllerSpec.scala
@@ -8,30 +8,14 @@ class ControllerSpec extends AnyWordSpec {
   "The controller" when {
     val field = new Field(3, 3)
     val controller = new Controller(field)
-    "setBombs-function is called" should {
-      "have the same number of bombs in the field as field.setBombs" in {
-        controller.setBombs(3)
-        field.setBombs(3)
-        var countC: Int = 0
-        for (i <- (0 until field.rows))
-          for (j <- (0 until field.cols))
-            if (field.matrix.row(i)(j)._2 == Stone.Bomb)
-              countC = countC + 1
-        var countF: Int = 0
-        for (i <- (0 until field.rows))
-          for (j <- (0 until field.cols))
-            if (field.matrix.row(i)(j)._2 == Stone.Bomb)
-              countF = countF + 1
-        countC should be(countF)
-      }
-    }
     "revealValue-function is called" should {
       "have the tuple elements swap positions" in {
-        controller.revealValue(0, 0) should not be (Stone.NotTracked)
+        controller.revealValue(Coordinates(0, 0)) should not be (Stone.NotTracked)
       }
     }
     "toString-function is called" should {
       controller.toString should be(field.toString)
     }
+
   }
 }
