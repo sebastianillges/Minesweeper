e019078c6950d2985926e5c3231944ccbf4c28f3
kaicikoglu <kaicikoglu@outlook.de>
2022-04-29 11:22:07 +0200

    created test for TUI

---
+++ src/test/scala/de/htwg/se/minesweeper/aview/TUISpec.scala
@@ -0,0 +1,16 @@
+package de.htwg.se.minesweeper.aview
+
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.minesweeper.model.*
+import de.htwg.se.minesweeper.controller.Controller
+
+class TUISpec extends AnyWordSpec {
+  "The TUI" should {
+    val tui = TUI(Controller(new Field(3, 3, (Stone.NotTracked, Stone.EmptyTracked))))
+    val field = new Field(3, 3, (Stone.NotTracked, Stone.EmptyTracked))
+    "recognize the input 00 as reveale value in the cell (0,0)" in {
+      tui.parseInput("00") should be(Some(field.revealValue(0, 0)))
+    }
+  }
+}
