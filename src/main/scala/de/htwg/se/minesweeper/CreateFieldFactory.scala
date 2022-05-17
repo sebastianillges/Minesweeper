693f606c250be4d77c43baa34231af9adb2863d8
kaicikoglu <kaicikoglu@outlook.de>
2022-05-17 19:14:10 +0200

    added Factory

---
+++ src/main/scala/de/htwg/se/minesweeper/CreateFieldFactory.scala
@@ -0,0 +1,27 @@
+package de.htwg.se.minesweeper
+
+import de.htwg.se.minesweeper.model.{Field, Matrix, Stone}
+
+trait CreateFieldFactory {
+  def run: Field = Field(matrix = new Matrix[Stone, Stone](8, 8, (Stone.NotTracked, Stone.EmptyTracked)))
+}
+
+private class Easy extends CreateFieldFactory {
+  override def run: Field = Field(matrix = new Matrix[Stone, Stone](8, 8, (Stone.NotTracked, Stone.EmptyTracked)))
+}
+
+private class Medium extends CreateFieldFactory {
+  override def run: Field = Field(matrix = new Matrix[Stone, Stone](16, 16, (Stone.NotTracked, Stone.EmptyTracked)))
+}
+
+private class Hard extends CreateFieldFactory {
+  override def run: Field = Field(matrix = new Matrix[Stone, Stone](32, 16, (Stone.NotTracked, Stone.EmptyTracked)))
+}
+
+object CreateFieldFactory {
+  def apply(kind: String) = kind match {
+    case "1" => new Easy()
+    case "2" => new Medium()
+    case "3" => new Hard()
+  }
+}
--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -1,3 +1,4 @@
+import de.htwg.se.minesweeper.CreateFieldFactory
 import de.htwg.se.minesweeper.aview.TUI
 import de.htwg.se.minesweeper.controller.Controller
 import de.htwg.se.minesweeper.model.*
@@ -9,12 +10,7 @@ import scala.io.StdIn.readLine
   println("Insert 1 for easy, 2 for medium or 3 for hard: \n")
 
   val input = readLine
-  var field: Option[Field] = None
-  input match
-    case "1" => field = Some(Field(matrix = new Matrix[Stone, Stone](8, 8, (Stone.NotTracked, Stone.EmptyTracked))))
-    case "2" => field = Some(Field(matrix = new Matrix[Stone, Stone](16, 16, (Stone.NotTracked, Stone.EmptyTracked))))
-    case "3" => field = Some(Field(matrix = new Matrix[Stone, Stone](32, 16, (Stone.NotTracked, Stone.EmptyTracked))))
-
-  val controller = Controller(field.get)
+  val createField = CreateFieldFactory(input.toString)
+  val controller = Controller(createField.run)
   val tui = TUI(controller)
   tui.run
