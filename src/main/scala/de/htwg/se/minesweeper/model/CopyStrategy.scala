dfbfa08aca4f2654c0d6295fda3ab5583932fc6b
kaicikoglu <kaicikoglu@outlook.de>
2022-05-17 17:55:39 +0200

    added factory and test

---
+++ src/main/scala/de/htwg/se/minesweeper/model/CopyStrategy.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.minesweeper.model
+
+case class CopyStrategy(field: Field, x: Int, y: Int, stone: Stone, stonePosition: Boolean) {
+  var strategy = if (stonePosition) then strategy1 else strategy2
+
+  def strategy1 =
+    field.put_1(stone, x, y)
+
+  def strategy2 =
+    field.put_2(stone, x, y)
+}
---
+++ src/test/scala/de/htwg/se/minesweeper/model/CopyStrategySpec.scala
@@ -0,0 +1,16 @@
+package de.htwg.se.minesweeper.model
+
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+
+class CopyStrategySpec extends AnyWordSpec {
+  "When CopyStrategy Pattern is used" should {
+    var field = new Field(3, 3)
+    field = CopyStrategy(field, 0, 0, Stone.Bomb, true).strategy
+    field = CopyStrategy(field, 0, 0, Stone.NotTracked, false).strategy
+    "replace the cell with the given stone, if true as parameter, then first tuple place else second" in {
+      field.matrix.rows(0)(0)._1 should be(Stone.Bomb)
+      field.matrix.rows(0)(0)._2 should be(Stone.NotTracked)
+    }
+  }
+}
