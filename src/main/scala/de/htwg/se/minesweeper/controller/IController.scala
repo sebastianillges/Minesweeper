0626565ae3d2891282c4e9968398743300eb5f98
kaicikoglu <kaicikoglu@outlook.de>
2022-05-30 16:41:39 +0200

    Components

---
+++ src/main/scala/de/htwg/se/minesweeper/controller/IController.scala
@@ -0,0 +1,18 @@
+package de.htwg.se.minesweeper.controller
+
+import de.htwg.se.minesweeper.model.{Coordinates, IField, Stone}
+import de.htwg.se.minesweeper.util.Observable
+
+trait IController extends Observable:
+  def doAndPublish(doThis: Coordinates => IField, coordinates: Coordinates): Unit
+  def doAndPublish(doThis: => IField): Unit
+  def quit(): Unit
+  def getCell(coordinates: Coordinates): (Stone, Stone, Int)
+  def calculateBombAmount(): Int
+  def revealValue(move: Coordinates): IField
+  def undo: IField
+  def redo: IField
+  def noStep(move: Coordinates): IField
+  def setBombs(bombAmount: Int): IField
+  def setFlag(coordinates: Coordinates): IField
+  def field: IField
---
+++ src/main/scala/de/htwg/se/minesweeper/model/IField.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.minesweeper.model
+
+trait IField:
+  val cols: Int
+  val rows: Int
+  def firstBar(cellWidth: Int, row: Int): String
+  def bar(cellWidth: Int, row: Int): String
+  def lastBar(cellWidth: Int, row: Int): String
+  def cells(row: Int, cellWidth: Int): String
+  def matchfield(cellWidth: Int = 3): String
+  def getCell(x: Int, y: Int): (Stone, Stone, Int)
+  def setBombs(bombNumber: Int): IField
+  def revealValue(x: Int, y: Int): IField
+  def setFlag(x: Int, y: Int): IField
+  def detectBombs(): Map[Coordinates, Boolean]
+  def detectFlags(): Map[Coordinates, Boolean]
+  def calculateBombAmount(): Int
+  def putValues(): IField
+  def showValues(): IField
+  def matrix: Matrix[Stone, Stone, Int]
+  override def toString: String = matchfield()
