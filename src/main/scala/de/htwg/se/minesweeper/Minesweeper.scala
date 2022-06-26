0092f1404e128b769c2e02207d8bed1f3b4f4c21
kaicikoglu <kaicikoglu@outlook.de>
2022-06-26 14:48:20 +0200

    Test

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -9,13 +9,12 @@ import scala.util.{Failure, Success, Try}
 
 @main def main(): Unit =
 
-  println("Insert 1 for easy, 2 for medium or 3 for hard: \n")
-
   println(
-    "\n To reveal a Field Cell type in the cooridinates as for example: 00, \n" +
-      " if you want to place a Flag in this Field write a f behind the cooridnates"
+    "Insert 1 for easy, 2 for medium or 3 for hard: \n" +
+      "To reveal a Field Cell type in the cooridinates as for example: 00, \n" +
+      "if you want to place a Flag in this Field write a f behind the cooridnates \n" +
+      "Press q to exit, u to undo and r to redo your move"
   )
-  println("\n Press q to exit, u to undo and r to redo your move")
   val injector = Guice.createInjector(new MinesweeperModuleEasy)
   val controller = injector.getInstance(classOf[ControllerInterface])
   val tui = TUI(controller)
